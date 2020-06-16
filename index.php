5e8ece7333a8b10e71c4d7548a0c4730bcba14cb
tunggiang123 <tunggiang123@gmail.com>
2020-06-16 18:27:55 +0700

    1

---
+++ index.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<title></title>
+	<link rel="stylesheet" type="text/css" href="./css/style.css">
+</head>
+<body>
+	<div style="float: left; width: 700px;">
+		<?php 
+				// 1. Chuỗi kết nối đến CSDL
+		$ket_noi = mysqli_connect("remotemysql.com:3306", "MFjxl5C769", "4yl0CnAF1W", "MFjxl5C769");
+		// 2. Viết câu lệnh SQL để lấy ra dữ liệu mong muốn
+		$sql = "
+		SELECT *
+		FROM chu_de
+		";
+		// 3. Thực hiện truy vấn để lấy ra các dữ liệu mong muốn
+		$chu_de = mysqli_query($ket_noi, $sql);
+		// 4. Hiển thị dữ liệu mong muốn
+		while ($row = mysqli_fetch_array($chu_de)) {
+			;?>
+			<table>
+				<tr>					
+					<td style="vertical-align: top;"><p c><i><?php echo $row["ten_chu_de"];?></i></p></td>
+				</tr>
+			</table>
+			<hr>
+			<?php
+		}
+		;?>
+		<div>
+
+		</div>	
+	</body>
+	</html>
